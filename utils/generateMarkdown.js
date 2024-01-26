44ef2df490c03b8b6aea99a6177c3627d27365bf
akent <alexanderkent099@gmail.com>
2024-01-26 14:00:47 -0500

    Add a function to generate markdown for README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,10 +13,37 @@ function renderLicenseSection(license) {
     return '## License\nThis project is licensed under the ${license} license. More information can be found at [${license}](${renderLicenseLink(license)}).';
 }
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents 
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Questions](#questions)
+- [License](#license)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For any questions, please reach out to me.
+
+${renderLicenseSection(data.license)}
 `;
 }
 
