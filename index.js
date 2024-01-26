4eb00bdfb774ad98308ba4ff2288e96b10631ff4
akent <alexanderkent099@gmail.com>
2024-01-26 14:13:18 -0500

    Add a function to write README file

--- index.js
+++ index.js
@@ -51,8 +51,12 @@ const questions = [
     },
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) throw err;
+        console.log('Successfully created README.md!');
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
